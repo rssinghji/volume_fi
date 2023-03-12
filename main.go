9d26714f921b559c923f6cb298d8bef8bf59bdf5
Ravneet Singh <ravneetsingh@Ravneets-MBP.attlocal.net>
2023-03-12 01:44:00 -0600

    Initial commit for GRPC server
    Signed-off-by: Ravneet Singh <ravneetsingh@Ravneets-MBP.attlocal.net>

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net"
 	"net/http"
 	"runtime"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	api "volumefi.com/volume_fi/api"
+	pb "volumefi.com/volume_fi/pb"
 )
 
 func main() {
@@ -17,6 +22,33 @@ func main() {
 	http.HandleFunc("/", api.HandleDefault)
 	http.HandleFunc("/calculate", api.HandleCalculateFlightTracker)
 
+	// Start GRPC Servre
+	go startGRPCServer()
+
 	// Start the server on port 8080
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+type server struct {
+	pb.FlightServer
+}
+
+func (s *server) Calculate(ctx context.Context, in *pb.CalculatePathRequest) (*pb.CalculatePathResponse, error) {
+	return &pb.CalculatePathResponse{
+		Response: &pb.Response{FlightPath: []string{"SFO", "ATL", "GSO", "IND", "EWR"}},
+	}, nil
+}
+
+func startGRPCServer() {
+	listener, err := net.Listen("tcp", ":8089")
+	if err != nil {
+		panic(err)
+	}
+
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+	pb.RegisterFlightServer(grpcServer, &server{})
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
